cab34d98793ec68a54233379dca10d4e5949dcf2
shijib <shijib@sino-bridge.com>
2019-08-28 15:55:57 +0800

    恢复

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("master");
-        System.out.println("local");
-        System.out.println("dev");
-        System.out.println("add local");
     }
 }

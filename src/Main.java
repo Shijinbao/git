4264c8c5ac22fb21572b6c8beda18bfec95e1141
shijib <shijib@sino-bridge.com>
2019-08-28 15:08:38 +0800

    只剩一行

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("欢天喜地过大年");
-        System.out.println("第一次提交");
         System.out.println("第二次提交");
     }
 }

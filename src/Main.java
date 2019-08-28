e3ba320f5e8adfad4704a079617473044e55319a
shijib <shijib@sino-bridge.com>
2019-08-28 15:07:00 +0800

    第二次提交

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("看看这次提交成功了 吗");
         System.out.println("欢天喜地过大年");
         System.out.println("第一次提交");
+        System.out.println("第二次提交");
     }
 }

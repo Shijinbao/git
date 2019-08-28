5f01fa60034e040989ca4b17e57a43815d0f13f7
shijib <shijib@sino-bridge.com>
2019-08-28 15:45:33 +0800

    归一

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("master");
-        System.out.println("local");
-        System.out.println("咯cal");
-        System.out.println("local新增一行");
-        System.out.println("111");
     }
 }

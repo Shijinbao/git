f3ad4521f506ffa76f6ff33ade710c6ce6a21a1c
shijib <shijib@sino-bridge.com>
2019-09-09 16:11:05 +0800

    only dev

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("master");
-        System.out.println("master1");
-        System.out.println("master2");
-        System.out.println("3");
-        System.out.println("4");
-        System.out.println("5");
-        System.out.println("dev");
+        System.out.println("only dev");
     }
 }
